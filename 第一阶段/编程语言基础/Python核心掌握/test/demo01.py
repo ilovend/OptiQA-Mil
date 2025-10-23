95b309b3654fc2bbf0817ab6dbaa8e35a2080d24
ilovend <ilovendme@outlook.com>
2025-10-23 18:59:44 +0800

    today end

---
+++ 第一阶段/编程语言基础/Python核心掌握/test/demo01.py
@@ -0,0 +1,34 @@
+# import keyword
+# print("Hello, World!")
+
+
+# def is_valid_number(name):
+#     try:
+#         exec(f"{name} = None")  # 尝试将 None 分配给变量名
+#         return True
+#     except:
+#         return False
+# print(is_valid_number("var1"))  # True
+# print(is_valid_number("1var"))  # False
+
+# print(keyword.kwlist)  # True
+
+# if False:
+#     print("true")
+# else:
+#     print("false")
+
+# item_one = 1
+# item_two = 2
+# item_three = 3
+# total = item_one + \
+#         item_two + \
+#         item_three
+# print(total)
+
+# str="123456789"
+
+# print(str[0:-1])
+
+# print(str[1:5:2])
+
